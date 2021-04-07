a4126b626e0d086df902d4eea3af6984b5d414bb
Kyoko <kyokoconnect@gmail.com>
2021-04-06 22:34:46 -0400

    1st commit, passing 3

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = (31)
+const num2 = (2)
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Number.isInteger (Number > 0) 
+
+// don't understand test result
+//AssertionError: expected false to be true
+//       + expected - actual
+//       -false
+//       +true
+    
+
+
+const num3 = (16)
+const num4 = (6)
+const mod = (num3 % num4)
+
+const max = Math.max (1, 7, 15, 20)
+    
